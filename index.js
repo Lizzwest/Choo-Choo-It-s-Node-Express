869e67ddc8e730ea5f9fa8b92ce695a40dea6986
Lizz <lizwesterband@gmail.com>
2020-08-05 14:07:53 -0700

    [add] Add ejs files

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require("express")
 
 const app = express()
 
+//tell 
+app.set('view engine', 'ejs')
+
+
+
 app.get('/about', function(req, res) {
-    res.sendFile(__dirname+'/views/about.html')
+    //res.sendFile(__dirname+'/views/about.html')
+    res.render('about')
   });
 app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/views/index.html');
+    //res.sendFile(__dirname+'/views/index.html');
+    //ejs file with this name
+    res.render('index')
   });
   
 
   app.get("/blog", (req, res)=>{
-    res.sendFile(__dirname+'/views/blog-directory.html')
+   // res.sendFile(__dirname+'/views/blog-directory.html')
+   res.render('blog-directory')
 })
 
 
